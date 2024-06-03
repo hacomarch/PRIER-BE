859266b0279051d3df36c42736fe5af2a51a16cc
hacomarch <haco_march@naver.com>
2024-06-03 11:42:19 +0900

    feat: 카카오 로그인 구현 및 유저 정보 h2 DB에 저장

---
+++ src/main/java/cocodas/prier/user/KakaoService.java
@@ -0,0 +1,77 @@
+package cocodas.prier.user;
+
+import cocodas.prier.user.dto.response.KakaoTokenResponseDto;
+import cocodas.prier.user.dto.response.KakaoUserInfoResponseDto;
+import io.netty.handler.codec.http.HttpHeaderValues;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class KakaoService {
+    private final UserRepository userRepository;
+    @Value("${kakao.client_id}")
+    private String client_id;
+
+    @Value(("${kakao.redirect_uri}"))
+    private String redirect_uri;
+
+    @Value("${kakao.client_secret}")
+    private String client_secret;
+    private final String KAUTH_TOKEN_URL_HOST = "https://kauth.kakao.com";
+    private final String KAUTH_USER_URL_HOST = "https://kapi.kakao.com";
+
+    public String getAccessToken(String code) {
+        KakaoTokenResponseDto kakaoTokenResponseDto = WebClient.create(KAUTH_TOKEN_URL_HOST).post()
+                .uri(uriBuilder -> uriBuilder
+                        .scheme("https")
+                        .path("/oauth/token")
+                        .queryParam("grant_type", "authorization_code")
+                        .queryParam("client_id", client_id)
+                        .queryParam("redirect_uri", redirect_uri)
+                        .queryParam("code", code)
+                        .queryParam("client_secret", client_secret)
+                        .build(true)
+                ).header(HttpHeaders.CONTENT_TYPE, HttpHeaderValues.APPLICATION_X_WWW_FORM_URLENCODED.toString())
+                .retrieve()
+                .bodyToMono(KakaoTokenResponseDto.class)
+                .block();
+
+        return kakaoTokenResponseDto.getAccessToken();
+    }
+
+    public KakaoUserInfoResponseDto getUserInfo(String accessToken) {
+        KakaoUserInfoResponseDto userInfo = WebClient.create(KAUTH_USER_URL_HOST).get()
+                .uri(uriBuilder -> uriBuilder
+                        .scheme("https")
+                        .path("/v2/user/me")
+                        .build(true))
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken)
+                .header(HttpHeaders.CONTENT_TYPE, HttpHeaderValues.APPLICATION_X_WWW_FORM_URLENCODED.toString())
+                .retrieve()
+                .bodyToMono(KakaoUserInfoResponseDto.class)
+                .block();
+
+        if (isDuplicateEmail(userInfo.getKakaoAccount().email)) {
+            Users user = Users.builder()
+                    .email(userInfo.getKakaoAccount().email)
+                    .nickname(userInfo.getKakaoAccount().getProfile().nickName)
+                    .build();
+            userRepository.save(user);
+        }
+
+        return userInfo;
+    }
+
+    private boolean isDuplicateEmail(String email) {
+        Users users = userRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("이메일이 없습니다."));
+
+        return users == null;
+    }
+}
