290ef5bb54b10a643b501b6f1aca2f5a49a06aaa
saokiritoni <leesoeun2746@dgu.ac.kr>
2024-06-05 14:42:16 +0900

    feat: 피드백 기간 연장 (FEEDBACK_EXTENSION) 기능 PointTransactionService 추가

--- src/main/java/cocodas/prier/point/PointTransactionService.java
+++ src/main/java/cocodas/prier/point/PointTransactionService.java
@@ -2,6 +2,7 @@ package cocodas.prier.point;
 
 import cocodas.prier.point.dto.PointRechargeRequest;
 import cocodas.prier.point.dto.PointTransactionDTO;
+import cocodas.prier.project.project.Project;
 import cocodas.prier.user.UserRepository;
 import cocodas.prier.user.Users;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -45,11 +46,38 @@ public class PointTransactionService {
                 .users(user)
                 .build();
 
-        user.setBalance(user.getBalance() + request.getAmount());
+        user.updateBalance(request.getAmount());
         userRepository.save(user);
         pointTransactionRepository.save(transaction);
 
         return convertToDto(transaction);
+    }
+
+    // 피드백 기간 연장 (FEEDBACK_EXTENSION)
+    public PointTransactionDTO extendFeedbackPeriod(Long userId, Long projectId, int weeks) {
+        Users user = userRepository.findById(userId).orElseThrow();
+        Project project = projectRepository.findById(projectId).orElseThrow(); // project 수정 후 재수정 예정
+
+        int cost = weeks * 500 // 1주당 500 포인트로 설정
+        if (user.getBalance() < cost) { // Users 수정후 재수정 예정
+            throw new IllegalArgumentException("포인트가 부족합니다.");
+        }
+
+        project.extendFeedbackPeriod(weeks);
+        user.updateBalance(-cost);
+
+        PointTransaction transaction = PointTransaction.builder()
+                .amount(-cost)
+                .transactionType(TransactionType.FEEDBACK_EXTENSION)
+                .createdAt(LocalDateTime.now())
+                .balance(user.getBalance()) // Users 수정후 재수정 예정
+                .users(user)
+                .build();
+
+        userRepository.save(user);
+        projectRepository.save(project); // project 수정 후 재수정 예정
+        pointTransactionRepository.save(transaction);
+
     }
 
     private PointTransactionDTO convertToDto(PointTransaction transaction) {
