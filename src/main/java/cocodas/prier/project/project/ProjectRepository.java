ecfc1c45c0cbaac0b9c5c30104513ca4520243b4
popeye0618 <popeye0618@gmail.com>
2024-06-07 15:35:52 +0900

    feat : 프로젝트를 키워드로 검색해서 조회하는 기능 구현
    프로젝트 제목, 팀 이름, 태그를 검색해서 조회 가능

--- src/main/java/cocodas/prier/project/project/ProjectRepository.java
+++ src/main/java/cocodas/prier/project/project/ProjectRepository.java
@@ -1,8 +1,20 @@
 package cocodas.prier.project.project;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProjectRepository extends JpaRepository<Project, Long> {
+
+    @Query("SELECT DISTINCT p FROM Project p " +
+            "LEFT JOIN p.projectTags pt " +
+            "LEFT JOIN pt.tag t " +
+            "WHERE LOWER(p.title) LIKE LOWER(CONCAT('%', :keyword, '%')) " +
+            "OR LOWER(p.teamName) LIKE LOWER(CONCAT('%', :keyword, '%')) " +
+            "OR LOWER(t.tagName) LIKE LOWER(CONCAT('%', :keyword, '%'))")
+    List<Project> findByKeyword(@Param("keyword") String keyword);
 }
