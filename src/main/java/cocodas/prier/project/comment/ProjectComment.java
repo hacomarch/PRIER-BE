f4d379575a17884081d86f3d431df84687c8b8e6
popeye0618 <popeye0618@gmail.com>
2024-06-08 13:51:59 +0900

    feat : 프로젝트 댓글 CRUD 구현 / 프로젝트 별점 구현
    댓글다는건 퀘스트랑도 연결되야할듯.

--- src/main/java/cocodas/prier/project/comment/ProjectComment.java
+++ src/main/java/cocodas/prier/project/comment/ProjectComment.java
@@ -3,19 +3,28 @@ package cocodas.prier.project.comment;
 import cocodas.prier.project.project.Project;
 import cocodas.prier.user.Users;
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.cglib.core.Local;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
+@NoArgsConstructor
 public class ProjectComment {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long commentId;
+
+    @Column(nullable = false)
     private String content;
-    private Integer score;
-    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private Float score;
+
+    private LocalDateTime createdAt = LocalDateTime.now();
     private LocalDateTime updatedAt;
 
     @ManyToOne(fetch = FetchType.LAZY)
@@ -25,4 +34,24 @@ public class ProjectComment {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private Users users;
+
+    @Builder
+    public ProjectComment(String content, Float score, Project project, Users users) {
+        this.content = content;
+        this.score = score;
+        this.project = project;
+        this.users = users;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setScore(Float score) {
+        this.score = score;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
