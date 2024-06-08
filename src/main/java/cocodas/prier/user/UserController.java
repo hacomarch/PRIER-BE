f3e06e79262422a02df39835874541ac08513876
hacomarch <haco_march@naver.com>
2024-06-08 16:13:00 +0900

    test: 개발할 때 jwt 토큰 필요해서 테스트용 추가

--- src/main/java/cocodas/prier/user/UserController.java
+++ src/main/java/cocodas/prier/user/UserController.java
@@ -5,9 +5,11 @@ import cocodas.prier.user.kakao.KakaoService;
 import cocodas.prier.user.dto.response.LoginSuccessResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
@@ -18,6 +20,20 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
     private final UserService userService;
     private final KakaoService kakaoService;
+
+    //TODO : 테스트용 나중에 지워야 함
+    @Value("${kakao.get_code_path}")
+    private String getCodePath;
+    @Value("${kakao.client_id}")
+    private String client_id;
+    @Value(("${kakao.redirect_uri}"))
+    private String redirect_uri;
+    @GetMapping("/kakao/login")
+    public String loginPage(Model model) {
+        String location = getCodePath + client_id + "&redirect_uri=" + redirect_uri;
+        model.addAttribute("location", location);
+        return "login";
+    }
 
     @GetMapping("/kakao/callback")
     public ResponseEntity<LoginSuccessResponse> callback(@RequestParam("code") String code) {
