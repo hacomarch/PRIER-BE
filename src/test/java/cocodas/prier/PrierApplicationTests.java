25fe17ac4763ea7caa7bf710e8aa0c076915ac09
hacomarch <haco_march@naver.com>
2024-06-10 18:19:26 +0900

    chore: 테스트 코드 삭제

--- src/test/java/cocodas/prier/PrierApplicationTests.java
+++ src/test/java/cocodas/prier/PrierApplicationTests.java
@@ -1,35 +1,9 @@
 package cocodas.prier;
 
-import cocodas.prier.user.kakao.jwt.token.RefreshTokenService;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 public class PrierApplicationTests {
-	private final RefreshTokenService refreshTokenService;
 
-	@Autowired
-	public PrierApplicationTests(RefreshTokenService refreshTokenService) {
-		this.refreshTokenService = refreshTokenService;
-	}
-
-	@Test
-	public void testTokenSaveAndFind() {
-		Long userId = 1L;
-		String refreshToken = "sample-refresh-token";
-
-		// Save the token
-		refreshTokenService.saveRefreshToken(userId, refreshToken);
-
-		// Find the token
-		Long foundUserId = refreshTokenService.findIdByRefreshToken(refreshToken);
-		System.out.println("Found UserId: " + foundUserId);
-
-		// Assertions to check if the saved and found user IDs match
-		assertEquals(userId, foundUserId);
-	}
 }
 
