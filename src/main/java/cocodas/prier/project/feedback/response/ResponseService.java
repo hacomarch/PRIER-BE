e4687e1864d87cba74f8eacbf95512fecf17fb3e
kimseonhee126 <kimseonhee126@gmail.com>
2024-06-20 16:25:17 +0900

    fix: 프로젝트 응답 상세보기

--- src/main/java/cocodas/prier/project/feedback/response/ResponseService.java
+++ src/main/java/cocodas/prier/project/feedback/response/ResponseService.java
@@ -1,9 +1,21 @@
 package cocodas.prier.project.feedback.response;
 
+import cocodas.prier.project.comment.ProjectCommentService;
+import cocodas.prier.project.feedback.question.Category;
 import cocodas.prier.project.feedback.question.Question;
 import cocodas.prier.project.feedback.question.QuestionRepository;
+import cocodas.prier.project.feedback.response.dto.ResponseDetailDto;
 import cocodas.prier.project.feedback.response.dto.ResponseDto;
+import cocodas.prier.project.feedback.response.dto.ResponseObjectiveDto;
 import cocodas.prier.project.feedback.response.dto.ResponseRequestDto;
+import cocodas.prier.project.project.Project;
+import cocodas.prier.project.project.ProjectRepository;
+import cocodas.prier.project.project.ProjectService;
+import cocodas.prier.statics.chatgpt.ChatGPTService;
+import cocodas.prier.statics.chatgpt.response.SummaryResponse;
+import cocodas.prier.statics.keywordAi.KeywordsService;
+import cocodas.prier.statics.keywordAi.dto.response.KeyWordResponseDto;
+import cocodas.prier.statics.objective.ObjectiveResponseService;
 import cocodas.prier.user.Users;
 import cocodas.prier.user.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -11,10 +23,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 @RequiredArgsConstructor
@@ -24,6 +36,13 @@ public class ResponseService {
     private final ResponseRepository responseRepository;
     private final QuestionRepository questionRepository;
     private final UserRepository userRepository;
+    private final ProjectRepository projectRepository;
+
+    private final ProjectService projectService;
+    private final KeywordsService keywordsService;
+    private final ObjectiveResponseService objectiveResponseService;
+    private final ChatGPTService chatGPTService;
+    private final ProjectCommentService projectCommentService;
 
     @Transactional
     public List<ResponseDto> createResponses(Long userId, List<ResponseRequestDto> responsesDto) {
@@ -76,6 +95,90 @@ public class ResponseService {
                 .map(this::mapToDto)
                 .collect(Collectors.toList());
     }
+
+    /*
+    프로젝트(프로젝트 이름, 팀 이름, 프로젝트 상세내용, 깃허브 링크 or 배포링크)
+    AI 키워드 분석
+    제출된 피드백 개수
+    객관식 통계
+    상세응답 분석
+        - 주관식은 chatGPT 요약 2줄
+        - 객관식은 10 ~ 50까지 개수 count
+    해당 프로젝트의 댓글(내용, 작성자, 별점, 소속)
+    * */
+    public ResponseDetailDto viewResponseDetail(Long projectId, Long userId) {
+
+        Users users = userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("사용자가 없습니다."));
+
+        Project project = projectRepository.findById(projectId).orElseThrow(() -> new IllegalArgumentException("프로젝트가 없습니다"));
+
+        List<KeyWordResponseDto> keywordByProjectId = keywordsService.getKeywordByProjectId(projectId);
+
+        int[] feedbackAmount = projectService.getFeedbackAmount(project);
+
+        Double percentage = objectiveResponseService.calculateFeedbackPercentage(projectId);
+
+        List<SummaryResponse> chatGptResponse = chatGPTService.getChatGptResponse(projectId);
+
+        List<ResponseObjectiveDto> objectiveByProject = getObjectiveByProject(projectId);
+
+        return new ResponseDetailDto(
+                projectId,
+                project.getTitle(),
+                project.getIntroduce(),
+                project.getTeamName(),
+                project.getLink(),
+                keywordByProjectId,
+                feedbackAmount[2],
+                String.format("%.2f", percentage),
+                chatGptResponse,
+                objectiveByProject,
+                projectCommentService.getProjectComments(userId),
+                users.getBalance()
+        );
+    }
+
+    public List<ResponseObjectiveDto> getObjectiveByProject(Long projectId) {
+        List<Question> questions = projectRepository.findById(projectId)
+                .map(project -> project.getFeedbackQuestions()
+                        .stream()
+                        .filter(question -> question.getCategory().equals(Category.OBJECTIVE))
+                        .toList())
+                .orElse(Collections.emptyList());
+
+        List<ResponseObjectiveDto> objectiveCountList = new ArrayList<>();
+
+        for (Question question : questions) {
+            long veryGoodCount = question.getResponses().stream()
+                    .filter(response ->response.getContent().equals("10"))
+                    .count();
+            long goodCount = question.getResponses().stream()
+                    .filter(response ->response.getContent().equals("20"))
+                    .count();
+            long sosoCount = question.getResponses().stream()
+                    .filter(response ->response.getContent().equals("30"))
+                    .count();
+            long badCount = question.getResponses().stream()
+                    .filter(response ->response.getContent().equals("40"))
+                    .count();
+            long veryBadCount = question.getResponses().stream()
+                    .filter(response ->response.getContent().equals("50"))
+                    .count();
+
+            ResponseObjectiveDto dto = new ResponseObjectiveDto(
+                    question.getQuestionId(),
+                    (int) veryGoodCount,
+                    (int) goodCount,
+                    (int) sosoCount,
+                    (int) badCount,
+                    (int) veryBadCount
+            );
+
+            objectiveCountList.add(dto);
+        }
+        return objectiveCountList;
+    }
+
 
     @Transactional
     public void deleteResponses(Long projectId, Long userId) {
