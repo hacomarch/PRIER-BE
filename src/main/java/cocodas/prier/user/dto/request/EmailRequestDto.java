6be6500c03f804e7eaceb3eda4ef21358ceaaf84
kimseonhee126 <kimseonhee126@gmail.com>
2024-06-21 17:31:27 +0900

    fix: 이메일 수정하기

--- src/main/java/cocodas/prier/user/dto/request/EmailRequestDto.java
+++ src/main/java/cocodas/prier/user/dto/request/EmailRequestDto.java
@@ -2,9 +2,10 @@ package cocodas.prier.user.dto.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
-@AllArgsConstructor
+@NoArgsConstructor
 public class EmailRequestDto {
     private String email;
 }
