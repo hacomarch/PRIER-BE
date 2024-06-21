05515112ea3cf8a8c52b9f1152897e7c9f247f63
saokiritoni <leesoeun2746@dgu.ac.kr>
2024-06-21 18:23:50 +0900

    chore: 로그 및 기타 수정

--- src/main/java/cocodas/prier/project/feedback/response/ResponseService.java
+++ src/main/java/cocodas/prier/project/feedback/response/ResponseService.java
@@ -7,28 +7,34 @@ import cocodas.prier.project.feedback.response.dto.ResponseRequestDto;
 import cocodas.prier.user.Users;
 import cocodas.prier.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
+@Slf4j
 public class ResponseService {
 
     private final ResponseRepository responseRepository;
     private final QuestionRepository questionRepository;
     private final UserRepository userRepository;
 
+    private static final String INVALID_USER_ID_MESSAGE = "Invalid user ID: ";
+    private static final String INVALID_QUESTION_ID_MESSAGE = "Invalid question ID: ";
+
     @Transactional
     public List<ResponseDto> createResponses(Long userId, List<ResponseRequestDto> responsesDto) {
         Users user = userRepository.findById(userId)
-                .orElseThrow(() -> new IllegalArgumentException("Invalid user ID"));
+                .orElseThrow(() -> new IllegalArgumentException(INVALID_USER_ID_MESSAGE + userId));
 
         List<Long> questionIds = responsesDto.stream()
                 .map(ResponseRequestDto::getQuestionId)
@@ -40,10 +46,8 @@ public class ResponseService {
 
         List<Response> responses = responsesDto.stream()
                 .map(dto -> {
-                    Question question = questionMap.get(dto.getQuestionId());
-                    if (question == null) {
-                        throw new IllegalArgumentException("Invalid question ID" + dto.getQuestionId());
-                    }
+                    Question question = Optional.ofNullable(questionMap.get(dto.getQuestionId()))
+                            .orElseThrow(() -> new IllegalArgumentException(INVALID_QUESTION_ID_MESSAGE + dto.getQuestionId()));
 
                     return Response.builder()
                             .content(dto.getContent())
@@ -55,23 +59,24 @@ public class ResponseService {
                 .collect(Collectors.toList());
 
         responseRepository.saveAll(responses);
+        log.info("Responses saved for user Id: {}", userId);
 
         return responses.stream()
                 .map(this::mapToDto)
                 .collect(Collectors.toList());
     }
 
-    public List<ResponseDto> getResponsesByQuestion(Long questionId) {
-        List<Response> responses = responseRepository.findAllByQuestionQuestionId(questionId);
-
-        return responses.stream()
-                .map(this::mapToDto)
-                .collect(Collectors.toList());
-    }
+//    public List<ResponseDto> getResponsesByQuestion(Long questionId) {
+//        List<Response> responses = responseRepository.findAllByQuestionQuestionId(questionId);
+//
+//        return responses.stream()
+//                .map(this::mapToDto)
+//                .collect(Collectors.toList());
+//    }
 
     public List<ResponseDto> getResponsesByProject(Long projectId) {
         List<Response> responses = responseRepository.findAllByQuestionProjectProjectId(projectId);
-
+        log.info("Response for project ID: {}", projectId);
         return responses.stream()
                 .map(this::mapToDto)
                 .collect(Collectors.toList());
@@ -81,18 +86,22 @@ public class ResponseService {
     public void deleteResponses(Long projectId, Long userId) {
         List<Response> responses = responseRepository.findAllByQuestionProjectProjectIdAndUsersUserId(projectId, userId);
         responseRepository.deleteAll(responses);
+        log.info("Responses deleted for project ID: {} and user ID: {}", projectId, userId);
     }
 
     public List<Long> getProjectsByUser(Long userId) {
         List<Long> projectIds = responseRepository.findDistinctProjectIdsByUserId(userId);
+        log.info("Project IDs retrieved for user ID: {}", userId);
         return projectIds;
     }
 
     public long countFeedbackForUserProjectsAfterLastLogin(Long userId) {
         LocalDateTime lastLoginAt = userRepository.findById(userId)
-                .orElseThrow(() -> new IllegalArgumentException("Invalid user ID"))
+                .orElseThrow(() -> new IllegalArgumentException(INVALID_USER_ID_MESSAGE + userId))
                 .getLastLoginAt();
-        return responseRepository.countFeedbackForUserProjectsAfterLastLogin(userId, lastLoginAt);
+        long feedbackCount = responseRepository.countFeedbackForUserProjectsAfterLastLogin(userId, lastLoginAt);
+        log.info("Feedback count after last login for user ID: {} is {}", userId, feedbackCount);
+        return feedbackCount;
     }
 
     private ResponseDto mapToDto(Response response) {
